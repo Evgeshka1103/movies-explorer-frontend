60d736350cd4de3a6041ae0109fad417a39dea4f
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-08-30 11:20:02 +0300

    test

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -85,8 +85,9 @@ export default function MoviesCardList({
           />
         ))}
       </ul>
+
       <div className="movies-card-block-button">
-        { location.pathname === '/saved-movies' && (moviesContent <= filterCheckTime.length) ? null : (
+      { location.pathname === '/movies' && moviesContent >= filterCheckTime.length ? null : (
           <button
             className="movies-card-buton-content"
             onClick={handleContentMovies}
