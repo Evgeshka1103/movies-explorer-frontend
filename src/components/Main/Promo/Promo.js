5658d10a287afde47755b1400ee176f8c942dd40
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-08-31 13:03:51 +0300

    test

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -3,13 +3,15 @@ import NavTab from "../NavTab/NavTab";
 
 export default function Promo() {
   return (
-    <section className="promo">
 
+    <section className="promo">
+<div className="promo__container">
         <h1 className="promo__title">
           Учебный проект студента факультета Веб-разработки.
         </h1>
 
-
+<NavTab />
+</div>
     </section>
 
   );
